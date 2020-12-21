0441b5ae02836adc2861cbafd1fe2ce1a39a6911
tlips <thomas.lips@ugent.be>
2020-12-21 23:09:25 +0100

    create repo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("startup");
+        rb = GetComponent<Rigidbody>();      
+    }
+
+    void OnMove(InputValue movementValue){
+        Debug.Log("onmove");
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX =  movementVector.x;
+        movementY = movementVector.y;
+        Debug.Log("x =" + movementY + "y=" + movementY);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement =  new Vector3(movementX,0.0f,movementY);
+        rb.AddForce(movement*speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
